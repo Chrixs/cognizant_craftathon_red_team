d73e6d7cca77297c07d8288e04ab695f3eadad93
Mike Field-May <mfieldmay@gmail.com>
2017-04-04 16:56:51 +0100

    Add search by date functionality to insights

--- personal_finance_managment/app/controllers/transactions_controller.rb
+++ personal_finance_managment/app/controllers/transactions_controller.rb
@@ -6,7 +6,21 @@ class TransactionsController < ApplicationController
 
   def show
     @transactions = Transaction.all
+    if check_filter(:date)
+      date = format_date(params[:date])
+      @transactions = Transaction.where(date: date)
+    end
     @total = 0.00
+  end
+
+  private
+
+  def check_filter(filter)
+    params[filter].present?
+  end
+
+  def format_date(date)
+    date.split('-').reverse.join('/')
   end
 
 end
--- personal_finance_managment/spec/features/insights_feature_spec.rb
+++ personal_finance_managment/spec/features/insights_feature_spec.rb
@@ -5,15 +5,27 @@ require 'pry'
 feature 'insights' do
   context 'insights' do
     scenario 'should have a branded header' do
-      visit '/transactions/show'
+      visit '/transactions/insights'
       expect(page).to have_content 'Team Red Bank'
       expect(page).to have_selector("nav")
     end
 
     scenario 'should return total spent' do
       add_transaction_history
-      visit '/transactions/show'
+      visit '/transactions/insights'
       expect(page).to have_content 'Total: £285.5'
     end
+
+    scenario 'can see total spent on specific days' do
+      add_transaction_history
+      visit '/transactions/insights'
+      fill_in 'date', with: '03/04/2017'
+      click_button 'Filter by Date'
+      within(:css, 'div#insights-display') do
+        expect(page).to have_content('£12.5')
+      end
+    end
   end
+
+
 end

179fee4f0e62176c66e8582776f662a145b71d26
Mike Field-May <mfieldmay@gmail.com>
2017-04-04 13:48:52 +0100

    Add navbar to index page

--- personal_finance_managment/spec/index_spec.rb
+++ personal_finance_managment/spec/index_spec.rb
@@ -2,6 +2,11 @@ require 'rails_helper'
 
 feature 'transactions' do
   context 'transactions' do
+    scenario 'should have a branded header' do
+      visit '/transactions'
+      expect(page).to have_content 'Team Red Bank'
+      expect(page).to have_selector("nav")
+    end
     scenario 'should have no transactions' do
       visit '/transactions'
       expect(page).to have_content 'No transactions yet'

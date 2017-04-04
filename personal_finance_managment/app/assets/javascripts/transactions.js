2c768183e1508e81ce61f5713f4bf0af574c8fbf
kathicks <kmhicks92@gmail.com>
2017-04-04 15:49:55 +0100

    Add colour coding for categories

--- personal_finance_managment/app/assets/javascripts/transactions.js
+++ personal_finance_managment/app/assets/javascripts/transactions.js
@@ -1,11 +1,10 @@
-
 var statusChanger = (function(){
    var statusColor = function() {
-     $(".category:contains('Bills')").attr("bg-color", "red");
-     $(".category:contains('Transport')").attr("bg-color", "yellow");
-     $(".category:contains('Groceries')").attr("bg-color", "green");
-     $(".category:contains('Shopping')").attr("bg-color", "green");
-     $(".category:contains('Food and drink')").attr("bg-color", "green");
+     $(".category:contains('Bills')").attr("bgcolor", "$pink");
+     $(".category:contains('Transport')").attr("bgcolor", "yellow");
+     $(".category:contains('Groceries')").attr("bgcolor", "green");
+     $(".category:contains('Shopping')").attr("bgcolor", "green");
+     $(".category:contains('Eat')").attr("bgcolor", "green");
  };
 
  return {

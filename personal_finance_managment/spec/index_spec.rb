37d63a9aad3e81d62e803bd59eb1cc85e8e8cb53
Samir Gossain <samir.gossain@gmail.com>
2017-04-04 14:45:13 +0100

    Returns Transactions on transactions/index

--- personal_finance_managment/spec/index_spec.rb
+++ personal_finance_managment/spec/index_spec.rb
@@ -1,10 +1,18 @@
 require 'rails_helper'
+require 'web_helpers'
 
 feature 'transactions' do
   context 'transactions' do
-    scenario 'should have no transactions' do
+    scenario 'should display empty History of Transactions' do
       visit '/transactions'
-      expect(page).to have_content 'No transactions yet'
+      expect(page).to have_content 'Transactions Company Amount Date Category'
     end
+
+    scenario 'displays History of Transactions' do
+      add_transaction_history
+      visit'/transactions'
+      expect(page).to have_content 'British Gas £12.5 2017-04-03 Bills'
+    end
+
   end
 end
